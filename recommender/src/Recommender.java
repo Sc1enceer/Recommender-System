ce61ef490efc2f88cd5d5b954370fbc53fc0523a
ECSAlvin <35316956+ECSAlvin@users.noreply.github.com>
2020-03-07 21:47:32 +0000

    created entities and coded eu distance similarity function

--- recommender/src/Recommender.java
+++ recommender/src/Recommender.java
@@ -1,1 +1,1 @@
-public class Recommender {    }
+import entity.Item;import entity.User;import java.util.Set;public class Recommender {    private Set<User> users;    private Set<Item> items;}
---
+++ recommender/src/entity/Item.java
@@ -0,0 +1,1 @@
+package entity;public class Item {    private int itemId;    public Item(int itemId) {        this.itemId = itemId;    }    public int getItemId() {        return itemId;    }    @Override    public String toString() {        return "item [itemId=" + itemId + "]";    }}
