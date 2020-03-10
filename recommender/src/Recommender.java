c7108e0aa3a8a45737a69eb7e5fab417fd04de50
ECSAlvin <35316956+ECSAlvin@users.noreply.github.com>
2020-03-10 02:23:29 +0000

    some stuff

--- recommender/src/Recommender.java
+++ recommender/src/Recommender.java
@@ -1,1 +1,1 @@
-import entity.Item;import entity.User;import java.util.Set;public class Recommender {    private Set<User> users;    private Set<Item> items;}
+import entity.Item;import entity.User;import java.util.Set;public class Recommender {    private Set<User> users;    private Set<Item> items; }
