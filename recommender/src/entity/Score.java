28ff4d1fdfa39a71dd9244da389b4b4b711ec134
Jacob-Neale <47900120+Jacob-Neale@users.noreply.github.com>
2020-03-22 23:22:42 +0000

    Add files via upload

--- recommender/src/entity/Score.java
+++ recommender/src/entity/Score.java
@@ -1,1 +1,27 @@
-package entity;public class Score {    private Item item;    private double score;    public Score(Item item, double score) {        this.item = item;        this.score = score;    }    public Item getItem() {        return item;    }    public void setItem(Item item) {        this.item = item;    }    public double getScore() {        return score;    }    public void setScore(double score) {        this.score = score;    }}
+package entity;
+
+public class Score {
+    private Item item;
+    private double score;
+
+    public Score(Item item, double score) {
+        this.item = item;
+        this.score = score;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+}
